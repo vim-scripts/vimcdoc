4a73ff283c538ad9684e6cb9b4b462c2999e7327
Wenzhi Liang <wenzhi.liang@gmail.com>
2004-07-07 00:00:00 +0000

    Version 0.7.0: Fully compatible with the new Vim 6.3.

--- vimcdoc.sh
+++ vimcdoc.sh
@@ -1,45 +1,69 @@
 #!/bin/sh
 #
-# vimcdoc.sh:	vimcdoc Linux install/uninstall script 
+# vimcdoc.sh:	vimcdoc Linux install/uninstall script
 #
-# Usage: 	(run it as root)
-#		'./vimcdoc.sh install' to install vimcdoc
-#        	'./vimcdoc.sh uninstall' to uninstall vimcdoc
+# Usage: 		(run it as root)
+#				'./vimcdoc.sh install' to install vimcdoc
+#				'./vimcdoc.sh uninstall' to uninstall vimcdoc
 #
-# Author:	wandys  (wandys@users.sf.net)
-#		lang2	(lang2@users.sf.net)
+# Author:		wandys	(wandys@users.sf.net)
+#				lang2	(lang2@users.sf.net)
 
-vim --cmd ":exec 'normal i' . \$VIMRUNTIME | wq! /tmp/vimrt.$$" > /dev/null 2>&1 
+# change these if you needed for your system
+INSTALLPRG=install
+RMPRG=rm
+LNPRG=ln
 
-if [ -e /tmp/vimrt.$$ ]; then
-	VIM_PATH=`cat /tmp/vimrt.$$`
-	rm /tmp/vimrt.$$
+
+VIMCDOC_PATH=/usr/share/doc/vimcdoc-`cat VERSION`
+VIMCDOC_PATH_LINK=/usr/share/doc/vimcdoc
+CNTAGS=tags-cn
+DIST_FILES="README
+guides.txt
+LICENSE
+TODO
+VERSION
+INSTALL
+AUTHORS
+dict.txt"
+VIMCDOC_INST='vimcdoc.sh'
+
+install_dist_files=0
+
+if [ `whoami` == 'root' ]; then
+	VIM_PATH=/usr/share/vim/vimfiles
+	install_dist_files=1
 else
-	echo 'Error: No vim found on this system.'
-	exit 1
+	VIM_PATH=$HOME/.vim/
 fi
 
 case $1 in
-  install)
-	if [ ! -d $VIM_PATH/doc.bk ]; then
-		mkdir $VIM_PATH/doc.bk
-		cp $VIM_PATH/doc/* $VIM_PATH/doc.bk
+	install)
+	if [ $install_dist_files -eq 1 ]; then
+		for i in $DIST_FILES; do
+			$INSTALLPRG -D -m 644 $i $VIMCDOC_PATH/$i
+		done
+		$INSTALLPRG -D -m 755 $VIMCDOC_INST $VIMCDOC_PATH/$VIMCDOC_INST
+		$RMPRG -f $VIMCDOC_PATH_LINK
+		$LNPRG -s $VIMCDOC_PATH $VIMCDOC_PATH_LINK
 	fi
-	if [ -f /etc/debian_version ]; then
-		cp doc/help.txt $VIM_PATH/doc
-		gzip doc/*.txt
-	fi
-	cp -r doc/* $VIM_PATH/doc
+	cd doc
+	for i in *.cnx; do
+		$INSTALLPRG -D -m 644 $i $VIM_PATH/doc/$i
+	done
+	$INSTALLPRG -D -m 644 $CNTAGS $VIM_PATH/doc/$CNTAGS
 	echo 'Done.'
 	;;
 
-  uninstall)
-	if [ -d $VIM_PATH/doc.bk ]; then
-		mv $VIM_PATH/doc.bk/* $VIM_PATH/doc
-		rmdir $VIM_PATH/doc.bk
+	uninstall)
+	if [ $install_dist_files -eq 1 ]; then
+		$RMPRG -rf $VIMCDOC_PATH
+		$RMPRG -f $VIMCDOC_PATH_LINK
 	fi
-	if [ -d $VIM_PATH/doc/CVS ]; then
-		rm -rf $VIM_PATH/doc/CVS
+	if [ -d $VIM_PATH/doc ]; then
+		cd $VIM_PATH/doc
+		$RMPRG -f *.cnx
+		$RMPRG -f $CNTAGS
 	fi
 	echo 'Done.'
 	;;
@@ -50,5 +74,5 @@ case $1 in
 	;;
 esac
 
-# vim:ts=8:
+# vim:ts=4:sw=4:
 # EOF
